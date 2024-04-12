79c5f5c5250745c16a1caf4f89dac2710693a3fe
Heather Dice <heatherdice22@gmail.com>
2024-04-12 16:03:07 -0400

    Add JS comments

--- day2/day2.js
+++ day2/day2.js
@@ -1,8 +1,11 @@
 // Day 2 animations
+
+// add event listener when content loads
 document.addEventListener("DOMContentLoaded", () => {
 	// select DOM elements
 	const menu = document.querySelector(".hamburger");
 
+	// onclick, remove static class & add animation class; if animation class is already present, remove it
 	menu.addEventListener("click", ()  => {
 		menu.classList.remove("static");
 		menu.classList.toggle("animation");
