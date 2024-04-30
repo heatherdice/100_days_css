0c27280c106850c2ed5a4648f64c210023078b50
Heather Dice <heatherdice22@gmail.com>
2024-04-30 13:06:01 -0400

    Got App to render

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import React from "react";
+
+export default function App() {
+    return (
+        <div className="App">
+            <footer>
+            <p>
+                <a href="https://github.com/heatherdice/100_days_css">Code</a> by Heather Dice
+            </p>
+            </footer>
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
+import ReactDOM from "react-dom";
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
-import App from "./App";
+import App from "./App.js";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
