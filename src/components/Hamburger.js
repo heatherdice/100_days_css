f2a2b35966466acfd1a9a369737ce02f39a74adf
Heather Dice <heatherdice22@gmail.com>
2024-05-01 18:06:50 -0400

    Added function for hamburger animation

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../styles/Hamburger.css";
 
 export default function Hamburger() {
+    // toggle classes for animations on hamburger click
+    useEffect(() => {
+        // select DOM element
+        const menu = document.querySelector(".hamburger");
+
+        // onclick, remove static class & add animation class; if animation class is already present, remove it
+        const handleClick = () => {
+            menu.classList.remove("static");
+            menu.classList.toggle("animation");
+        };
+
+        // add event listener when component mounts
+        menu.addEventListener("click", handleClick);
+
+        // remove event listener when component unmounts
+        return () => {
+            menu.removeEventListener("click", handleClick);
+        };
+    }, []);
+
     return (
         <div class="hamburger static">
 			<div class="top-line"></div>
