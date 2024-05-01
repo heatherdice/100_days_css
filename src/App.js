11bb47e3e04aa0d685431efcd2d807f346c26370
Heather Dice <heatherdice22@gmail.com>
2024-05-01 13:47:13 -0400

    Added hero styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import React from "react";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 export default function App() {
     return (
         <div className="App">
             <Navbar />
+            <Hero />
             <footer>
             <p>
                 <a href="https://github.com/heatherdice/100_days_css">Code</a> by Heather Dice
---
+++ src/components/Hero.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/Hero.css";
+
+export default function Hero() {
+    return (
+        <header>
+            <h1>100 Days of CSS</h1>
+        </header>
+    )
+}
