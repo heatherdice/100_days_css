b8fb3ef4abbcb48ca9dd8b9152f0b85fa6054d3e
Heather Dice <heatherdice22@gmail.com>
2024-05-06 15:43:39 -0400

    Code cleanup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Days from "./components/Days";
 
 export default function App() {
     return (
@@ -10,6 +11,7 @@ export default function App() {
             <Navbar />
             <Hero />
             <About />
+            <Days />
             <footer>
                 <p className="footer-text">
                     <a href="https://github.com/heatherdice/100_days_css">Code</a> by Heather Dice
