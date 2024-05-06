4f15c3acc7a368b6ef0dc55ad2358abaf37efc5d
Heather Dice <heatherdice22@gmail.com>
2024-05-06 15:55:57 -0400

    Code cleanup

--- src/components/days/Day1.js
+++ src/components/days/Day1.js
@@ -3,28 +3,26 @@ import "../../styles/dayStyles/Day1.css";
 
 export default function Day1() {
     return (
-        <>
-            {/* blue box container */}
-            <div className="container">
+        // blue box container
+        <div className="container">
 
-                {/* 100 */}
-                <div className="numbers">
-                    <div className="one-flag"></div>
-                    <div className="one-stem"></div>
-                    <div className="circle"></div>
-                    <div className="circle-two"></div>
-                </div>
-
-                {/* Days CSS Challenge text */}
-                <div className="center">
-                    <div className="text">
-                        <span className="days">DAYS</span>
-                        <br />
-                        <span className="css-challenge">CSS CHALLENGE</span>
-                    </div>
-                </div>
-                
+            {/* 100 */}
+            <div className="numbers">
+                <div className="one-flag" />
+                <div className="one-stem" />
+                <div className="circle" />
+                <div className="circle-two" />
             </div>
-        </>
+
+            {/* Days CSS Challenge text */}
+            <div className="center">
+                <div className="text">
+                    <span className="days">DAYS</span>
+                    <br />
+                    <span className="css-challenge">CSS CHALLENGE</span>
+                </div>
+            </div>
+            
+        </div>
     )
 }
