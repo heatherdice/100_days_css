ea3c6a44bbce6308b6756c86909834ea5afbb621
Heather Dice <heatherdice22@gmail.com>
2024-05-08 12:43:34 -0400

    2 columns in desktop view

--- src/components/Days.js
+++ src/components/Days.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "../styles/Days.css";
 import Day1 from "./days/Day1";
 import Day2 from "./days/Day2";
@@ -8,12 +8,12 @@ export default function Days() {
         <>
             <h2 className="days-title">Days</h2>
             <div className="days-columns">
-                <h3 className="day-heading">Day 1</h3>
                 <div className="col-1">
+                    <h3 className="day-heading">Day 1</h3>
                     <Day1 />
                 </div>
-                <h3 className="day-heading">Day 2</h3>
                 <div className="col-2">
+                    <h3 className="day-heading">Day 2</h3>
                     <Day2 />
                 </div>
             </div>
