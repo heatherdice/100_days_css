6a4d4de5ba3af9adb3f5458e471d3243f10dc5a4
Heather Dice <heatherdice22@gmail.com>
2024-05-15 14:51:43 -0400

    Added Day 3

--- src/components/Days.js
+++ src/components/Days.js
@@ -2,6 +2,7 @@ import React from "react";
 import "../styles/Days.css";
 import Day1 from "./days/Day1";
 import Day2 from "./days/Day2";
+import Day3 from "./days/Day3";
 
 export default function Days() {
     return (
@@ -11,6 +12,8 @@ export default function Days() {
                 <div className="col-1">
                     <h3 className="day-heading">Day 1</h3>
                     <Day1 />
+                    <h3 className="day-heading">Day 3</h3>
+                    <Day3 />
                 </div>
                 <div className="col-2">
                     <h3 className="day-heading">Day 2</h3>
---
+++ src/components/days/Day3.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../../styles/dayStyles/Day3.css";
+
+export default function Day3() {
+    return (
+        <>
+        {/* black box container */}
+        <div class="day3-container">
+        
+            {/* circle containing image */}
+            <div class="day3-circle">
+                <div class="sun"></div>
+                <div class="pyramid"></div>
+            </div>
+        </div>
+        </>
+    )
+}
