4feace35a73272cddef0dc2a64f9561155029c8e
Heather Dice <heatherdice22@gmail.com>
2024-05-15 17:57:52 -0400

    Improving grid layout

--- src/components/Days.js
+++ src/components/Days.js
@@ -9,18 +9,26 @@ export default function Days() {
     return (
         <>
             <h2 className="days-title">Days</h2>
-            <div className="days-containers">
-                <div className="col-1">
-                    <h3 className="day-heading">Day 1</h3>
-                    <Day1 />
-                    <h3 className="day-heading">Day 3</h3>
-                    <Day3 />
+            <div className="page-container">
+                <div className="grid-rows">
+                    <div className="grid-blocks">
+                        <h3 className="day-heading">Day 1</h3>
+                        <Day1 />
+                    </div>
+                    <div className="grid-blocks">
+                        <h3 className="day-heading">Day 2</h3>
+                        <Day2 />
+                    </div>
                 </div>
-                <div className="col-2">
-                    <h3 className="day-heading">Day 2</h3>
-                    <Day2 />
-                    {/* <h3 className="day-heading">Day 4</h3>
-                    <Day4 /> */}
+                <div className="grid-rows">
+                    <div className="grid-blocks">
+                        <h3 className="day-heading">Day 3</h3>
+                        <Day3 />
+                    </div>
+                    <div className="grid-blocks">
+                        {/* <h3 className="day-heading">Day 4</h3>
+                        <Day4 /> */}
+                    </div>
                 </div>
             </div>
         </>
