ce41c0c8a89c01400e9e83009ddf18ddd9c4f55d
Heather Dice <heatherdice22@gmail.com>
2024-05-22 15:16:11 -0400

    Consolidated navLinks objects

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -52,46 +52,16 @@ export default function Navbar() {
             title: 'Days',
             dropdown: true,
             subLinks: [
-                {
-                    link: '/1-10',
-                    title: 'Days 1-10'
-                },
-                {
-                    link: '/11-20',
-                    title: 'Days 11-20'
-                },
-                {
-                    link: '/21-30',
-                    title: 'Days 21-30'
-                },
-                {
-                    link: '/31-40',
-                    title: 'Days 31-40'
-                },
-                {
-                    link: '/41-50',
-                    title: 'Days 41-50'
-                },
-                {
-                    link: '/51-60',
-                    title: 'Days 51-60'
-                },
-                {
-                    link: '/61-70',
-                    title: 'Days 61-70'
-                },
-                {
-                    link: '/71-80',
-                    title: 'Days 71-80'
-                },
-                {
-                    link: '/81-90',
-                    title: 'Days 81-90'
-                },
-                {
-                    link: '/91-100',
-                    title: 'Days 91-100'
-                }        
+                {link: '/1-10', title: 'Days 1-10'},
+                {link: '/11-20', title: 'Days 11-20'},
+                {link: '/21-30', title: 'Days 21-30'},
+                {link: '/31-40', title: 'Days 31-40'},
+                {link: '/41-50', title: 'Days 41-50'},
+                {link: '/51-60', title: 'Days 51-60'},
+                {link: '/61-70', title: 'Days 61-70'},
+                {link: '/71-80', title: 'Days 71-80'},
+                {link: '/81-90', title: 'Days 81-90'},
+                {link: '/91-100', title: 'Days 91-100'}        
             ]
         },
         {
