e807e63486318db55c955313e426a00b1cc71676
Heather Dice <heatherdice22@gmail.com>
2024-06-13 12:12:49 -0400

    Generated index file

---
+++ src/components/dayComponents/index.js
@@ -0,0 +1,17 @@
+import Day1 from './Day1.js';
+import Day2 from './Day2.js';
+import Day3 from './Day3.js';
+import Day4 from './Day4.js';
+import Day5 from './Day5.js';
+import Day6 from './Day6.js';
+import Day7 from './Day7.js';
+
+export default [
+    { component: Day1, title: "Day 1" },
+    { component: Day2, title: "Day 2" },
+    { component: Day3, title: "Day 3" },
+    { component: Day4, title: "Day 4" },
+    { component: Day5, title: "Day 5" },
+    { component: Day6, title: "Day 6" },
+    { component: Day7, title: "Day 7" }
+];
