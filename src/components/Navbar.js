c2f6b02a62a5cc5018d03bcf36514e362309a329
Heather Dice <heatherdice22@gmail.com>
2024-09-05 17:09:00 -0400

    Trying new approach to dropdown, fixed close-dropdown class issue (though clunky)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useLayoutEffect, useEffect, useCallback } from "react";
 import _ from "lodash";
 import { Link } from 'react-scroll';
 // import { NavLink } from "react-router-dom";
@@ -14,48 +14,58 @@ export default function Navbar() {
     // mobile menu open state, initial value set to closed
     const [dropdownOpen, setDropdownOpen] = useState(false);
 
-    // delay handleResize function until 300ms after last resize event, preventing constant function calls
-    const handleResize = useCallback(_.debounce(() => {
-        // determine screen type upon resizing
-        setScreenType(window.innerWidth <= 600 ? 'mobile' : 'desktop');
-        // test
-        console.log(screenType);
+    // // delay handleResize function until 300ms after last resize event, preventing constant function calls
+    // const handleResize = useCallback(_.debounce(() => {
+    //     // determine screen type upon resizing
+    //     setScreenType(window.innerWidth <= 600 ? 'mobile' : 'desktop');
+    //     // test
+    //     console.log(screenType);
 
-        // close dropdown on resize
-        if(dropdownOpen) {
-            setDropdownOpen(false);
-        }
-    }, 300), [screenType, dropdownOpen]); // ensure latest state values are used
+    //     // close dropdown on resize
+    //     if(dropdownOpen) {
+    //         setDropdownOpen(false);
+    //     }
+    // }, 300), [screenType, dropdownOpen]); // ensure latest state values are used
 
-    // run eventListeners when handleResize is called
-    useEffect(() => {
-        window.addEventListener('resize', handleResize);
+    // // run eventListeners when handleResize is called
+    // useEffect(() => {
+    //     window.addEventListener('resize', handleResize);
         
+    //     return () => {
+    //         window.removeEventListener('resize', handleResize);
+    //     };
+    // }, [handleResize]);
+    
+    // // open/close mobile menu
+    // const toggleDropdown = () => {
+    //     setDropdownOpen(prevState => !prevState);
+    //     console.log("TOGGLED DROPDOWN");
+    // };
+
+    const handleResize = () => {
+        const newScreenType = window.innerWidth <= 600 ? 'mobile' : 'desktop';
+        setScreenType(newScreenType);
+    };
+
+    useLayoutEffect(() => {
+        window.addEventListener('resize', handleResize);
+        handleResize();
+
         return () => {
             window.removeEventListener('resize', handleResize);
         };
     }, [handleResize]);
-    
-    // open/close mobile menu
+
     const toggleDropdown = () => {
-        setDropdownOpen(prevState => !prevState);
-        console.log("TOGGLED DROPDOWN");
-    };
+        setDropdownOpen(!dropdownOpen);
+    }
+
 
     const navLinks = [
-        {
-            link:'about',
-            title: 'About'
-        },
-        {
-            link: 'days',
-            title: 'Days'
-        },
-        {
-            link: 'learn-more',
-            title: 'Learn More'
-        }
-    ]
+        {link:'about', title: 'About'},
+        {link: 'days', title: 'Days'},
+        {link: 'learn-more', title: 'Learn More'}
+    ];
 
     return (
         <>
@@ -80,10 +90,26 @@ export default function Navbar() {
                     </ul>
                 ) : (
                     // triggers toggleDropdown function onClick
-                    <Hamburger onClick={toggleDropdown} />
+                    <>
+                        <Hamburger onClick={toggleDropdown} />
+
+                        {dropdownOpen ? (
+                            <Dropdown 
+                                navLinks={navLinks} 
+                                isOpen={dropdownOpen} linkClick={toggleDropdown}
+                                className="open-dropdown" 
+                            />
+                        ) : (
+                            <Dropdown 
+                                navLinks={navLinks} 
+                                isOpen={dropdownOpen} 
+                                linkClick={toggleDropdown}
+                                className="close-dropdown" 
+                            />
+                        )}
+                    </>
                 )}
             </nav>
-            <Dropdown navLinks={navLinks} isOpen={dropdownOpen} linkClick={toggleDropdown} />
         </>
     )
 }
