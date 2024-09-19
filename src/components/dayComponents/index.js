0a052e223e07ae69d6a50d7b8d814a0d19879ceb
Heather Dice <heatherdice22@gmail.com>
2024-09-18 20:19:00 -0400

    Added components to index in correct order via script

--- src/components/dayComponents/index.js
+++ src/components/dayComponents/index.js
@@ -1,12 +1,13 @@
-import Day1 from '../../components/dayComponents/Day1.js';
-import Day2 from '../../components/dayComponents/Day2.js';
-import Day3 from '../../components/dayComponents/Day3.js';
-import Day4 from '../../components/dayComponents/Day4.js';
-import Day5 from '../../components/dayComponents/Day5.js';
-import Day6 from '../../components/dayComponents/Day6.js';
-import Day7 from '../../components/dayComponents/Day7.js';
-import Day8 from '../../components/dayComponents/Day8.js';
-import Day9 from '../../components/dayComponents/Day9.js';
+import Day1 from '../../components/dayComponents/Day01.js';
+import Day2 from '../../components/dayComponents/Day02.js';
+import Day3 from '../../components/dayComponents/Day03.js';
+import Day4 from '../../components/dayComponents/Day04.js';
+import Day5 from '../../components/dayComponents/Day05.js';
+import Day6 from '../../components/dayComponents/Day06.js';
+import Day7 from '../../components/dayComponents/Day07.js';
+import Day8 from '../../components/dayComponents/Day08.js';
+import Day9 from '../../components/dayComponents/Day09.js';
+import Day10 from '../../components/dayComponents/Day10.js';
 
 export default [
     { component: Day1, title: "Day 1" },
@@ -17,5 +18,6 @@ export default [
     { component: Day6, title: "Day 6" },
     { component: Day7, title: "Day 7" },
     { component: Day8, title: "Day 8" },
-    { component: Day9, title: "Day 9" }
+    { component: Day9, title: "Day 9" },
+    { component: Day10, title: "Day 10" }
 ];
