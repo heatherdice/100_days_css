4b54ac96eeb28cfa9c8ca2da0cbb0ddb80f41a9d
Heather Dice <heatherdice22@gmail.com>
2024-09-19 11:37:00 -0400

    Added circles, date, time

--- src/components/dayComponents/Day10.js
+++ src/components/dayComponents/Day10.js
@@ -8,7 +8,18 @@ export default function Day10() {
             {/* clock face */}
             <div className="day10-outer-circle">
                 <div className="clock-face">
-                    
+                    <div className="moving-circle">
+                        <div className="dotted-circle">
+
+                            <div className="date">
+                                MON 15 JAN 2015
+                            </div>
+
+                            <div className="time">
+                                11:42
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
