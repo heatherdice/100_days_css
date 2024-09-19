9291f4f8a7a659021951bd1f6808159de23b69bf
Heather Dice <heatherdice22@gmail.com>
2024-09-19 11:14:00 -0400

    Added circles to Day 10

--- src/components/dayComponents/Day10.js
+++ src/components/dayComponents/Day10.js
@@ -5,6 +5,12 @@ export default function Day10() {
     return (
         <div className="day-container container-color10">
             
+            {/* clock face */}
+            <div className="day10-outer-circle">
+                <div className="clock-face">
+                    
+                </div>
+            </div>
         </div>
     )
 }
