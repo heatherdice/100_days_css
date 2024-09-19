741c1fd75f59ae4c940d1e4e1cfd1d3ddcfcb46e
Heather Dice <heatherdice22@gmail.com>
2024-09-18 20:01:00 -0400

    Added Day 8

---
+++ src/components/dayComponents/Day8.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../../styles/dayStyles/Day8.css";
+
+export default function Day8() {
+    return (
+        <div className="day-container container-color8">
+            
+        </div>
+    )
+}
