2292f9d27e184663ac512d7b3ebfdf5502b9f9f0
Heather Dice <heatherdice22@gmail.com>
2024-09-18 20:10:00 -0400

    Added Day 10

---
+++ src/components/dayComponents/Day10.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../../styles/dayStyles/Day10.css";
+
+export default function Day10() {
+    return (
+        <div className="day-container container-color10">
+            
+        </div>
+    )
+}
