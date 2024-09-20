ce26981b9e5e71209a450b3108db0dc177a75577
Heather Dice <heatherdice22@gmail.com>
2024-09-20 13:33:00 -0400

    Simplified code, tried to get text & heart in row

--- src/components/dayComponents/Day10.js
+++ src/components/dayComponents/Day10.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "../../styles/dayStyles/Day10.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 
 export default function Day10() {
     return (
@@ -7,16 +9,27 @@ export default function Day10() {
             
             {/* clock face */}
             <div className="day10-circles day10-outer-circle">
-                <div className="day10-circles clock-face">
-                    <div className="day10-circles moving-circle">
-                        <div className="day10-circles dotted-circle">
+                <div className="day10-circles moving-circle">
+                    <div className="day10-circles dotted-circle">
 
-                            <div className="date">
-                                MON 15 JAN 2015
-                            </div>
+                        <div className="date">
+                            MON 15 JAN 2015
+                        </div>
 
-                            <div className="time">
-                                11:42
+                        <div className="time">
+                            11:42
+                        </div>
+
+                        <div className="health-stats">
+                            <div className="health-stats-row">
+                            <span className="heart-rate">
+                                <FontAwesomeIcon 
+                                    icon={faHeart} className="heart"
+                                />
+                                81
+                            </span>
+
+                            <span className="kcal"> 1248 KCAL</span>
                             </div>
                         </div>
                     </div>
