cf1d0abfebfb890dd175b175940a2d0df0318419
Heather Dice <heatherdice22@gmail.com>
2024-09-21 10:00:00 -0400

    Added sections to Day 9

--- src/components/dayComponents/Day09.js
+++ src/components/dayComponents/Day09.js
@@ -5,6 +5,15 @@ export default function Day09() {
     return (
         <div className="day-container container-color9">
             
+            {/* weather picture */}
+            <div className="weather-picture">
+
+            </div>
+            
+            {/* weather stats */}
+            <div className="weather-stats">
+                These are the weather stats
+            </div>
         </div>
     )
 }
