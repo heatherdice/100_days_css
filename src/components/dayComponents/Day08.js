bdd89189343c227e7543f4e5255a95c818b77084
Heather Dice <heatherdice22@gmail.com>
2024-09-22 13:44:00 -0400

    Added logic for smaller moving dots

--- src/components/dayComponents/Day08.js
+++ src/components/dayComponents/Day08.js
@@ -2,9 +2,22 @@ import React from "react";
 import "../../styles/dayStyles/Day08.css";
 
 export default function Day08() {
+    const dots = Array.from({ length: 8 }, (_, i) => (
+        <div key={`dot-${i + 1}`} className={`dot-${i + 1}`} />
+    ));
+
+    const spots = Array.from({ length: 10 }, (_, i) => (
+        <div key={`spots-${i + 1}`} className={`spots-${i + 1}`} />
+    ));
+
     return (
         <div className="day-container container-color8">
-            
+            <div className="ball">
+                <div className="white-center">
+                    {dots}
+                    {spots}
+                </div>
+            </div>
         </div>
     )
 }
