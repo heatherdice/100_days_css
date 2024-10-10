871f23082175dc080624ae7ebddf2f3df995d571
Heather Dice <heatherdice22@gmail.com>
2024-10-10 14:55:00 -0400

    Fixed point positioning, group positioning, added container

--- src/components/dayComponents/Day05.js
+++ src/components/dayComponents/Day05.js
@@ -39,20 +39,20 @@ export default function Day05() {
 
     // points positioning in line graph, value & color assignment
     const pointsData = [
-        { x: 9, y: 73, value: 458, color: 'red' },
-        { x: 51, y: 39, value: 812, color: 'red' },
-        { x: 90, y: 50, value: 746, color: 'red' },
-        { x: 130, y: 38, value: 877, color: 'red' },
-        { x: 171, y: 64, value: 517, color: 'red' },
-        { x: 211, y: 74, value: 434, color: 'red' },
-        { x: 251, y: 45, value: 458, color: 'red' },
-        { x: 9, y: 88, value: 26, color: 'blue' },
-        { x: 50, y: 78, value: 41, color: 'blue' },
-        { x: 90, y: 92, value: 22, color: 'blue' },
-        { x: 130, y: 82, value: 36, color: 'blue' },
-        { x: 171, y: 88, value: 25, color: 'blue' },
-        { x: 211, y: 101, value: 13, color: 'blue' },
-        { x: 251, y: 91, value: 20, color: 'blue' },
+        { x: 9, y: 45, value: 458, color: 'red' },
+        { x: 51, y: 13, value: 812, color: 'red' },
+        { x: 90, y: 23, value: 746, color: 'red' },
+        { x: 130, y: 12, value: 877, color: 'red' },
+        { x: 171, y: 38, value: 517, color: 'red' },
+        { x: 211, y: 48, value: 434, color: 'red' },
+        { x: 251, y: 19, value: 458, color: 'red' },
+        { x: 9, y: 61, value: 26, color: 'blue' },
+        { x: 50, y: 51, value: 41, color: 'blue' },
+        { x: 90, y: 64, value: 22, color: 'blue' },
+        { x: 130, y: 55, value: 36, color: 'blue' },
+        { x: 171, y: 61, value: 25, color: 'blue' },
+        { x: 211, y: 73, value: 13, color: 'blue' },
+        { x: 251, y: 64, value: 20, color: 'blue' },
     ];
 
     // render points on line graph
@@ -60,13 +60,15 @@ export default function Day05() {
         pointsData.map((point, index) => (
             <foreignObject
                 key={index}
-                x={point.x - 3} // center point horizontally
-                y={point.y - 30} // center the point vertically
-                width="100"
-                height="100"
+                x={point.x - 15} // center point horizontally
+                y={point.y - 15} // center the point vertically
+                width="30"
+                height="30"
             >
-                <div className={`points ${point.color}`}>
-                    <div className={`popup-bubble ${point.color}`}>{point.value}</div>
+                <div className="points-container">
+                    <div className={`points ${point.color}`}>
+                        <div className={`popup-bubble ${point.color}`}>{point.value}</div>
+                    </div>
                 </div>
             </foreignObject>
     ));
@@ -97,7 +99,7 @@ export default function Day05() {
 
             {/* card for chart */}
             <div className="chart-container">
-                
+
                 {/* chart header */}
                 <div className="chart-header">
                     <p className="chart-top-row">
