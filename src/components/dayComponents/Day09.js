f75759933780325b782d913491c9eb3b78fe17ef
Heather Dice <heatherdice22@gmail.com>
2026-01-16 16:25:46 -0500

    Raindrops added to JS

--- src/components/dayComponents/Day09.js
+++ src/components/dayComponents/Day09.js
@@ -8,7 +8,9 @@ export default function Day09() {
         triggerOnce: false,
         threshold: 0.2
     });
+
     const [animate, setAnimate] = useState(false);
+
     React.useEffect(() => {
         // toggle to reset animation
         if (inView) {
@@ -33,15 +35,56 @@ export default function Day09() {
         return <div key={`crater-${i + 1}`} className="crater" style={style} />
     });
 
+    // rain generation
+    const columns = 10;
+    const sizes = ["big", "medium", "small"];
+
+    const baseDurations = {
+        big: 0.7,
+        medium: 1.3,
+        small: 1.9
+    };
+
+    const raindrops = animate
+        ? Array.from({ length: columns }).flatMap((_, columnIndex) => sizes.map((size) => {
+            const left = -20 + columnIndex * 38;
+
+            const duration = 
+                baseDurations[size] + Math.random() * 0.2;
+            const delay = Math.random() * 2;
+
+            return (
+                <div
+                    key={`drop-${columnIndex}-${size}-${Math.random()}`}
+                    className={`drop ${size}`}
+                    style={{
+                        left: `${left}px`,
+                        animationDuration: `${duration}s`,
+                        animationDelay: `${delay}s`
+                    }}
+                />
+            );
+        })
+    )
+    : null;
+
     return (
         <div ref={ref} className="day-container container-color9">
             
             {/* top 3/4 w/ weather animation */}
             <div className="weather-picture">
+
+                {/* moon */}
                 <div className={`moon ${animate ? ' animate' : ''}`}>
                     {craters}
                 </div>
 
+                {/* raindrops */}
+                <div>
+                    {raindrops}
+                </div>
+
+                {/* hills */}
                 <div className="hills-container">
                     <div className="all-hills bg-hills hill-bg-1" />
                     <div className="all-hills bg-hills hill-bg-2" />
