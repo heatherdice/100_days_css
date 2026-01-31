9cb9e7dbde7617fa7333e4d8620c2b03a3ce3612
Heather Dice <heatherdice22@gmail.com>
2026-01-31 12:46:06 -0500

    Added Day 11 files

---
+++ src/components/dayComponents/Day11.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../../styles/dayStyles/Day11.css";
+
+
+export default function Day11() {
+    return (
+        <div className="day-container container-color11">
+
+        </div>
+    )
+}
