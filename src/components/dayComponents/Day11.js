5410f1a3affb63d1797521244ffdc8920f0978c0
Heather Dice <heatherdice22@gmail.com>
2026-02-02 09:04:30 -0500

    Established background of Day 11

--- src/components/dayComponents/Day11.js
+++ src/components/dayComponents/Day11.js
@@ -5,7 +5,10 @@ import "../../styles/dayStyles/Day11.css";
 export default function Day11() {
     return (
         <div className="day-container container-color11">
-
+            <div className="shoe-container">
+                
+            </div>
+            <div className="floor" />
         </div>
     )
 }
