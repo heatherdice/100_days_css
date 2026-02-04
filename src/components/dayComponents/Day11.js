bab6fe945c8d729d194d3d8cf5b5002f5eb222b8
Heather Dice <heatherdice22@gmail.com>
2026-02-04 09:42:41 -0500

    Began work on walking shoe animation

--- src/components/dayComponents/Day11.js
+++ src/components/dayComponents/Day11.js
@@ -6,7 +6,13 @@ export default function Day11() {
     return (
         <div className="day-container container-color11">
             <div className="shoe-container">
-                <div className="shoe" />
+                <div className="left-shoe">
+                    <div className="shoe" />
+                </div>
+
+                <div className="right-shoe">
+                    <div className="shoe" />
+                </div>
             </div>
             <div className="floor" />
         </div>
